b71d2bb8a48c0b8bf2f013ecea9d780d6883c5af
hisyamsk <hisyamskurniawan18@gmail.com>
2026-06-19 15:54:35 +0700

    feat(domain): add ledger domain

---
+++ internal/domain/ledger.go
@@ -0,0 +1,9 @@
+package domain
+
+type LedgerEntryType string
+
+const (
+	LedgerEntryTypeDebit  LedgerEntryType = "debit"
+	LedgerEntryTypeCredit LedgerEntryType = "credit"
+	LedgerEntryTypeDebit  LedgerEntryType = "debit"
+)
